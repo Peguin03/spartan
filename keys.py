651d73fa5557322b45ce759cd8dea5360a0bfd04
Peguin03 <45460179+Peguin03@users.noreply.github.com>
2021-05-06 08:31:06 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,13 +1,7 @@
-#keys={}
-#def keys():
-	#Enter your Twitter API keys here
-#	keys = {'consumer_key' : "",'consumer_secret' : "",'access_token_key' : "",'access_token_secret' : ""}
-	
-#	return keys
-	
 keys={}
 def keys():
-	
-	keys = {'consumer_key' : "7wuawE6suEG1bBGBeK2gMpmlO",'consumer_secret' : "Ge3RUS0P3R6vPuML3ER4QLiASMBDXYLlIhZ8lFbQUdNW8EZ6EK",'access_token_key' : "931516308822167552-GDPtC1s7EjPCDscRYo3WUtViYyxiZAj",'access_token_secret' : "pN0msKcQoBGaH32nwikd6fd3jTNHVIfCVJ5KdnkFyFLFb"}
+	Enter your Twitter API keys here
+	keys = {'consumer_key' : "",'consumer_secret' : "",'access_token_key' : "",'access_token_secret' : ""}
 	
 	return keys
+	
